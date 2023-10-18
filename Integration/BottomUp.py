9844c9bb9697b6943b2c1a3857b2cd7eb785fbc4
Mattia Bertacchini <matber2001@gmail.com>
2023-10-18 13:08:52 +0200

    Fixed bug during clustering

--- Integration/BottomUp.py
+++ Integration/BottomUp.py
@@ -123,7 +123,7 @@ def to_GMM(GMTA: pd.DataFrame):
 
 def schema_integration(sources, sim_methods, corr_method, score="SimAvg"):
     match_table = local_match_table(sources, sim_methods, corr_method, score)[['SLAT_A', 'SLAT_B']]
-    nodi = [col for df in sources.values() for col in df.columns]
+    nodi = [key + '_' + col for df, key in (sources.values(), sources.keys()) for col in df.columns]
     cluster = clustering_componenti_connessi(match_table, nodi)
     LAT = genera_lat(sources)
 
