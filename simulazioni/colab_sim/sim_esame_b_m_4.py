66c20811629da110d43c4374477244a479cc3af4
allePansera <allepansera2001@gmail.com>
2023-11-13 09:36:27 +0100

    Blocking & Matching exercise

---
+++ simulazioni/colab_sim/sim_esame_b_m_4.py
@@ -0,0 +1,121 @@
+from Blocking.EntityMatching import EntityMatching
+from Evaluation.Evaluation import vedi_valuta, valuta
+from ESERCIZI.utils.funzioni_prof import _VisualizzaDistribuzioneCluster, VisualizzaCluster
+import pandas as pd
+from copy import deepcopy
+
+path='http://dbgroup.ing.unimore.it/SIWS/DataIntegration/Esempi/BIKE/'
+
+src_links = [
+path+'BikeA.csv',
+path+'BikeB.csv']
+
+SOURCES = { 'S'+str(i+1) : pd.read_csv(link).astype(str) for i, link in enumerate(src_links) }
+
+GoldStandard=pd.read_csv(path+ 'BikeGS.csv')
+
+GoldStandard.columns=['l_id','r_id']
+
+
+
+# come prima cosa calcolo il cluster del golden standard
+cluster_gold_standard=EntityMatching.cluster_componenti_connessi(GoldStandard[['l_id','r_id']],
+                                                                 EntityMatching.id_sources(SOURCES))
+# visualizzo la cardinalità e la distro. del gold standard
+
+# visualizzare distribuzione del cluster -> printa in output
+_VisualizzaDistribuzioneCluster(cluster_gold_standard)
+
+# visualizzazione dei cluster
+grouped_cluster = VisualizzaCluster(cluster_gold_standard)
+
+
+# Creo una var. joint con bike_name e color
+for s in SOURCES:
+    # print(SOURCES[s].columns)
+    # creo un attributo che unisca due attributi per eseguire il blocking in sim join
+    SOURCES[s]['mix'] = (SOURCES[s]['bike_name']+' '+SOURCES[s]['city_posted']).str.lower()
+    # per un limite della mia scarsa capacità a programmare tolgo gli underscore
+    SOURCES[s]['BikeName'] = SOURCES[s]['bike_name'].str.lower()
+    SOURCES[s]['CityPosted'] = SOURCES[s]['city_posted'].str.lower()
+    SOURCES[s]['color'] = SOURCES[s]['color'].str.lower()
+
+
+# come primo tentativo per eseguire il matching provo ad usare un rules based matcher
+# blocking rules - blocco su fullname
+method = "join_blocker"
+# method = "rule_based_diff_type"
+blocking_keys = ["mix"]
+# blocking_keys = []
+blocking_rules = []
+# blocking_rules = [
+#    [{"rule": "{}_{}_jac_qgm_3_qgm_3(ltuple, rtuple) < {}", "attr": "fullname",
+#      "score": "0.2"}]]
+# matching rules
+matching_rules = [
+        [
+            {"rule": "{}_{}_jac_qgm_3_qgm_3(ltuple, rtuple) >= {}", "attr": "BikeName", "score": "0.45"},
+
+            {"rule": "{}_{}_jac_qgm_3_qgm_3(ltuple, rtuple) >= {}", "attr": "CityPosted", "score": "0.5"}
+            # potrei mettere degli exm
+        ]
+]
+# l attrs to exclude
+omit_l_attrs = ["l_id"]
+# r attrs to exclude
+omit_r_attrs = ["r_id"]
+# matching method
+matching_method = "SYMM" # STAB # SYMM
+
+em = EntityMatching(SOURCES)
+entity_match_table = em.process(method, blocking_keys, omit_l_attrs, omit_r_attrs,
+                                blocking_rules, matching_rules, matching_method)
+
+# query per verifica del matching all'interno della match table
+CorrespMT = entity_match_table.groupby('r_id')[['l_id']].count().sort_values('l_id', ascending=False)
+
+
+cluster_entity_matching=EntityMatching.cluster_componenti_connessi(entity_match_table[['l_id','r_id']],
+                                                                   EntityMatching.id_sources(SOURCES))
+
+# da controllare per verificare Precision e Recall
+Valuta = valuta(EntityMatching.calcola_match_indotti_cluster(cluster_gold_standard).rename(columns={"l_id": "A", "r_id": "B"}),
+                     EntityMatching.calcola_match_indotti_cluster(cluster_entity_matching).rename(columns={"l_id": "A", "r_id": "B"}))
+
+# da controllare quando si hanno score troppo bassi nel matching
+VediValuta_FN = vedi_valuta(EntityMatching.calcola_match_indotti_cluster(cluster_gold_standard).rename(columns={"l_id": "A", "r_id": "B"}),
+                     EntityMatching.calcola_match_indotti_cluster(cluster_entity_matching).rename(columns={"l_id": "A", "r_id": "B"}),
+                    "FN")
+
+VediValuta_FP = vedi_valuta(EntityMatching.calcola_match_indotti_cluster(cluster_gold_standard).rename(columns={"l_id": "A", "r_id": "B"}),
+                     EntityMatching.calcola_match_indotti_cluster(cluster_entity_matching).rename(columns={"l_id": "A", "r_id": "B"}),
+                    "FP")
+# da controllare per verificare il numero di elementi per ogni cluster, può essere comodo per valutare il mapping
+ClusterGrouping = cluster_entity_matching.groupby('ClusterKey').apply(EntityMatching.aggregazione_cluster).reset_index().sort_values('#Elements',
+                                                                                                 ascending=False)
+
+# visualizzare distribuzione del cluster
+_VisualizzaDistribuzioneCluster(cluster_entity_matching)
+
+# visualizzazione dei cluster
+grouped_cluster = VisualizzaCluster(cluster_entity_matching)
+
+
+Unione = pd.DataFrame(columns=SOURCES['S1'].columns)
+for x in SOURCES.keys():
+    Unione = Unione.append(SOURCES[x])
+vedi_valuta_detail_FN = pd.merge(pd.merge(VediValuta_FN.rename(columns={"A": "l_id", "B": "r_id"}),
+                                       Unione,
+                                       left_on='l_id', right_on='id'),
+                              Unione, left_on='r_id', right_on='id')
+
+vedi_valuta_detail_FP = pd.merge(pd.merge(VediValuta_FP.rename(columns={"A": "l_id", "B": "r_id"}),
+                                       Unione,
+                                       left_on='l_id', right_on='id'),
+                              Unione, left_on='r_id', right_on='id')
+
+exit(1)
+
+
+# vedi_valuta_detail_FP[['mix_x', 'mix_y']]
+# vedi_valuta_detail_FN[['mix_x', 'mix_y']]
